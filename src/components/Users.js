521cf62aaa580eda0177f9b4b316aac8d22fbae7
Patrick Coleman <pcole0083@gmail.com>
2016-10-19 23:20:26 -0400

    Show those users in a super basic way! upgrade to that scroll bar, at least for webkit.

--- src/components/Users.js
+++ src/components/Users.js
@@ -5,6 +5,7 @@ import * as API from '../api';
 //import only the Link property (Destructoring)
 import {Link} from 'react-router';
 import isAdmin from '../helpers/isAdmin';
+import ListItemWrapper from './ListItemWrapper';
 
 export default class Users extends React.Component {
     state = {
@@ -18,27 +19,26 @@ export default class Users extends React.Component {
     };
 
     componentWillUnmount(){
-        API.settings.off('value');
+        API.users.off('value');
         if(!!this.cleanup){
             this.cleanup()
         }
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        if(JSON.stringify(this.state.settings) === JSON.stringify(nextState.settings)){
-            return false;
-        }
-        return true;
-    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     if(JSON.stringify(this.state.settings) === JSON.stringify(nextState.settings)){
+    //         return false;
+    //     }
+    //     return true;
+    // }
 
-    componentWillReceiveProps(nextProps) {
-        console.log('next');
-        if(!!nextProps.user && (this.props.user.username !== nextProps.user.username) ){
-            let groups = nextProps.user.groups || {};
+    // componentWillReceiveProps(nextProps) {
+    //     if(!!nextProps.user && (this.props.user.username !== nextProps.user.username) ){
+    //         let groups = nextProps.user.groups || {};
 
-            this.updateState(nextProps.user, groups);
-        }
-    }
+    //         this.updateState(nextProps.user, groups);
+    //     }
+    // }
 
     componentWillUpdate(){
         let user = this.state.user;
@@ -61,14 +61,17 @@ export default class Users extends React.Component {
             }
 
             if(canViewPage) {
-                let mysettings = [];
-                // let mysettings = Object.keys(this.state.settings).map(id =>
-                //     <ListItem key={id+this.state.settings[id].value.toString().replace(/\s/gi, '_')} id={id} setting={this.state.settings[id]} className="setting-item" />
-                // );
+                let all_users = this.state.all_users.map((user) => {
+                    let groups = Object.keys(user.groups).map((id) => {
+                        return user.groups[id];
+                    });
+                    let text = 'Username: '+user.username+'  |  Groups: '+groups.join(', ');
+                    return <ListItemWrapper key={user.username} data={text} className="setting-item" />
+                });
                 return <article className="post-article admin-settings">
                     <h1>Site Users</h1>
                     <ul className="settings-group">
-                        {mysettings}
+                        {all_users}
                     </ul>
                     <div className="submit-wrapper">
                         <button type="button" onClick={this.updateUsers}>Update Users</button>
@@ -80,37 +83,38 @@ export default class Users extends React.Component {
     };
 
     updateState = (user, groups) => {
-        if(API.settings){
+        if(API.users){
             user = !!user ? user : this.state.user;
             groups = !!groups ? groups : this.state.groups;
 
             //API.settings.off('value');
-            API.settings.once('value', ss => {
+            API.users.once('value', ss => {
                 let json = ss.exportVal() || {},
-                    settings = Object.assign(this.state.settings, json);
+                    userKeys = Object.keys(json),
+                    users = userKeys.map((id) => {
+                        return json[id];
+                    });
 
                 this.setState({
-                    settings: settings,
+                    all_users: users,
                     user: user,
                     groups: groups
                 });
-
-                this.forceUpdate();
             });
         }
     }
 
-    updateSetting = evt => {
-        var ele = evt.target;
-        var name = ele.name,
-            type = ele.type,
-            updatedVal = ele.value;
+    // updateSetting = evt => {
+    //     var ele = evt.target;
+    //     var name = ele.name,
+    //         type = ele.type,
+    //         updatedVal = ele.value;
 
-        var updatedSetting = Object.assign(this.state.settings[name], {type: type, label: ele.getAttribute('data-lbl'), value: updatedVal});
+    //     var updatedSetting = Object.assign(this.state.settings[name], {type: type, label: ele.getAttribute('data-lbl'), value: updatedVal});
 
-        this.updateState();
-        API.settings.child(name).set(updatedSetting);
-    }
+    //     this.updateState();
+    //     API.settings.child(name).set(updatedSetting);
+    // }
 
     updateUsers = evt => {
         var ele = evt.target;
@@ -119,15 +123,15 @@ export default class Users extends React.Component {
         console.log(ele);
     }
 
-    renderOptions(setting) {
-        if(!setting || setting.type !== 'select-one'){
-            return null;
-        }
+    // renderOptions(setting) {
+    //     if(!setting || setting.type !== 'select-one'){
+    //         return null;
+    //     }
 
-        return setting.value.map((val, id) =>
-            <option key={id} value={setting.value[id].value} >{setting.value[id].label}</option>
-        );
-    };
+    //     return setting.value.map((val, id) =>
+    //         <option key={id} value={setting.value[id].value} >{setting.value[id].label}</option>
+    //     );
+    // };
 };
 
 Users.contextTypes = {
